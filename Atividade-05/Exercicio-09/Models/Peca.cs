cd4886894e71a376c71e9f2f3c082dcb357961a0
TheBud4 <murilopmr13@hotmail.com>
2024-08-06 16:13:04 -0300

    .

---
+++ Atividade-05/Exercicio-09/Models/Peca.cs
@@ -0,0 +1,6 @@
+namespace Exercicio_09.Models;
+
+public class Peca(string titulo, string diretor) {
+    public string Titulo { get; set; } = titulo;
+    public string Diretor { get; set; } = diretor;
+}
---
+++ Atividade-05/Exercicio-09/Program.cs
@@ -0,0 +1,25 @@
+﻿using Exercicio_09.Models;
+
+namespace Exercicio_09;
+
+class Program {
+    static void Main(string[] args) {
+        Teatro teatro = new();
+
+        teatro.AdicionarPeca( new Peca("O Rei Leão", "Nah Tvenya") );
+        teatro.AdicionarPeca( new Peca("O Fantasma da Ópera", "Andrew Lloyd Webber") );
+        teatro.AdicionarPeca( new Peca("Cats", "Andrew Lloyd Webber") );
+        
+        foreach (var peca in teatro.pecas) {
+            Console.WriteLine($"Nome: {peca.Titulo}\n" +
+                              $"Autor: {peca.Diretor}");
+        }
+        
+        teatro.RemoverPeca("O Fantasma da Ópera");
+        
+        foreach (var peca in teatro.pecas) {
+            Console.WriteLine($"Nome: {peca.Titulo}\n" +
+                              $"Autor: {peca.Diretor}");
+        }
+    }
+}
