61cbfdc8a36e4b94f7c7c89a792500a7e885ac7b
TheBud4 <murilopmr13@hotmail.com>
2024-08-14 18:17:30 -0300

    começando o projeto

--- ProjetoFinal/obj/Debug/net8.0/ProjetoFinal.AssemblyInfo.cs
+++ ProjetoFinal/obj/Debug/net8.0/ProjetoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8183a44b87c60664d68ad9750af9358eafa686cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b67bed7e5bf304812168afe0ea0d27fa84c148")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
